168dc626b268f9ba6970c32b023586841809135f
B-TRUNK <abanoub.boshra.anis@gmail.com>
2026-01-11 19:54:58 +0200

    classes

---
+++ _1_type_system/records/oop/NewOOP.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csodocs._1_type_system.records.oop
+{
+    public class NewOOP
+    {
+        
+    }
+}
